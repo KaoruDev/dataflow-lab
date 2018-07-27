a4b610c5de4e8a2404873f8d280ccfb65bb6236d
Kaoru Kohashigawa <Kaoru@siftscience.com>
2018-07-27 13:52:15 -0700

    write beam code

--- src/main/java/dataflowlab/ImagesPipeline1.java
+++ src/main/java/dataflowlab/ImagesPipeline1.java
@@ -32,46 +32,65 @@ import org.json.*;
 
 
 public class ImagesPipeline1 {
-	
-	private static final String PROJECT_ID =  "";
+
+	private static final String PROJECT_ID =  "qwiklabs-gcp-dbccb9a43fa6c950";
 	private static final String BUCKET_IN_PATH = "gs://"+PROJECT_ID+"-imagesin";
 	private static final String BUCKET_OUT_PATH = "gs://"+PROJECT_ID+"-imagesout";
 	private static final String TOPIC_URI =  "projects/"+PROJECT_ID+"/topics/iotdata";
-	
-	
+
+
 	public static class CopyImages extends DoFn<PubsubMessage, String>{
-		
+
 		CopyImages() {}
-	    
+
 	    @ProcessElement
 	    public void processElement(ProcessContext c) {
-			// TODO: complete the code here
-	    	//Parse the message and get filename and label
-	    	
-  	        //Build the ResourceIds and related list (for bucket in and bucket out)
-	  	    
-			// copy from bucket in to bucket out
-		    
-	    	// return created filename 
-            c.output("");
+			// parse the message and get filePath and label
+			String msg = new String(c.element().getPayload());
+			JSONObject msgJson = new JSONObject(msg);
+			String label = msgJson.getString("label");
+			label = label.trim().toLowerCase();
+			String pathToFileIn = msgJson.getString("filePath");
+			String fileName = "image.jpeg";
+
+			Instant timestamp = Instant.now();
+			String pathToFileOut = BUCKET_OUT_PATH + "/" + label + "/" + timestamp.toString() + "-copy-" + fileName;
+
+			List<ResourceId> listIn = new ArrayList<> ();
+			List<ResourceId> listOut = new ArrayList<> ();
+			listIn.add(FileSystems.matchNewResource(pathToFileIn, false));
+			listOut.add(FileSystems.matchNewResource(pathToFileOut, false));
+			// copy from in to out
+			try {
+				FileSystems.copy(listIn, listOut, StandardMoveOptions.IGNORE_MISSING_FILES);
+			}
+			catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+			c.output(fileName);
 	    }
-	    
+
 	}
-	
-	
+
+
 
 	public static void main(String[] args) {
 		// TODO: complete the main method
 		// Create PipelineOptions
-		
+		PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+
 		// Create Pipeline object
-		
+        Pipeline pipeline = Pipeline.create(options);
+
 		// Create PCOllection by reading incoming PubSub message (apply I/O Transform to the pipeline object)
-		
+		PCollection<PubsubMessage> pCollection =
+			pipeline.apply("read from PubSub", PubsubIO.readMessages().fromTopic(TOPIC_URI));
+
 		// Apply PTransform to the pipeline object with the DoFn class (copy incoming file from bucket in to bucket out)
-		
+		pCollection.apply("copy", ParDo.of(new CopyImages()));
+
 		// Run the pipeline
-		
+        pipeline.run();
 	}
 
 }
